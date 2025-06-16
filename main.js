ec03d61fbbe1065b4ebcc0dff1bd3b1f52bac860
Hussein <h.alsaleh95@gmail.com>
2025-06-16 16:45:22 +0200

    Adding getUsers to main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Fetching data from API Using XMLHTTPSREQUESt
 function getPosts() {
     let request = new XMLHttpRequest()
     request.open("GET", "https://jsonplaceholder.typicode.com/posts");
@@ -23,4 +24,36 @@ function getPosts() {
         }
     }
 }
+
+// Fetching users from API Using XMLHTTPSREQUESt
+
 getPosts();
+
+
+function getUsers() {
+    let request = new XMLHttpRequest()
+    request.open("GET", "https://jsonplaceholder.typicode.com/users");
+    request.responseType = "json";
+    request.send();
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 300) {
+            let users = request.response;
+            document.getElementById("users").innerHTML = "";
+
+            for (user of users) {
+                let content = `
+                <div id="user" class="selected">
+                    <h3>${user.username}</h3>
+                    <h3>${user.email}</h3>
+                </div>
+                        `;
+                document.getElementById("users").innerHTML += content;
+
+            }
+        } else {
+            alert("error");
+        }
+    }
+}
+
+getUsers();
